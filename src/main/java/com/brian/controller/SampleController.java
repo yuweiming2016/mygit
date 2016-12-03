33e33a7596851f6d24742c412018e7d532ffa033
Brian <yuweiming314@163.com>
2016-12-03 15:47:19 +0800

    create new project

---
+++ src/main/java/com/brian/controller/SampleController.java
@@ -0,0 +1,29 @@
+package com.brian.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class SampleController  {
+ 
+	@Value("${mysqldb.datasource.url:World!}")
+	private String name;
+	
+	
+    @ResponseBody
+    @RequestMapping(value = "/")
+    String home() { 
+
+        return "Hello World!" + name;
+    }
+ 
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(SampleController.class, args);
+    }
+ 
+}
